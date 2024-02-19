a09ded079d88fff0034edee16a19612963291b6d
vellabo <vellabo@ilstu.edu>
2024-02-19 15:51:12 -0600

    Round-2 commit with index.html and app.js

--- app.js
+++ app.js
@@ -3,46 +3,76 @@ const board = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0]
-]
+];
+
 const boardContainer = document.querySelector(".board");
-document.addEventListener('swiped-up', function(e){
-    console.log(e.traget);
+
+document.addEventListener('keyup', function (e) {
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        default:
+            return;
+    }
+    display();
+    assignRandom();
+    display();
 });
 
-function display(){
+function display() {
     let elem = 0;
-    for(let row =0; row<4; row++){
-        for(let col = 0; col< 4; col++){
-            if(board[row][col] === 0){
+    for (let row = 0; row < 4; row++) {
+        for (let col = 0; col < 4; col++) {
+            if (board[row][col] === 0) {
                 boardContainer.children[elem].style.color = "";
                 boardContainer.children[elem].innerText = "";
                 boardContainer.children[elem].style.backgroundColor = "";
-                elem++
-            } else{
-                if (board[row][col] >= 128){
+                elem++;
+            } else {
+                if (board[row][col] >= 128) {
                     boardContainer.children[elem].style.color = "white";
                 }
                 boardContainer.children[elem].innerText = board[row][col];
                 boardContainer.children[elem].style.backgroundColor = changeColor(row, col);
-                elem++
-            }
+                elem++;
             }
         }
     }
+}
 
-function assignRandom(){
-    let row = Math.floor(Math.rondom()* 4)
-    let col = Math.floor(Math.rondom()* 4)
-    if ( board[row][col] == 0) {
+function assignRandom() {
+    let row = Math.floor(Math.random() * 4);
+    let col = Math.floor(Math.random() * 4);
+    if (board[row][col] == 0) {
         let chance = Math.random();
         if (chance > 0.9)
-        board[row][col] = 4;
-    else board[row][col] = 2;
-    } else{
-        try{
+            board[row][col] = 4;
+        else
+            board[row][col] = 2;
+    } else {
+        try {
             assignRandom();
-        }catch{
+        } catch {
             console.log("game over");
         }
     }
 }
+
+function changeColor(row, col) {
+    let value = board[row][col];
+    return `hsla(220, ${(100 / 12) * (Math.log2(value) * 3)}%, ${(100 - Math.log2(value) * 12)}%, ${(100 - Math.log2(value) / 12)}%)`;
+}
+
+assignRandom();
+assignRandom();
+display();
